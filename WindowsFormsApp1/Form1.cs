c31330eb761a0b9c664f4c511d776ba22b6ab2aa
simoncicv.02@spst.eu <simoncicv.02@spst.eu>
2020-11-16 12:56:06 +0100

    součin

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -32,5 +32,13 @@ namespace WindowsFormsApp1
             int c = a - b;
             label1.Text = c.ToString();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            int c = a * b;
+            label1.Text = c.ToString();
+        }
     }
 }
